b661cf437647dc1d714842a878e732b198f46ee0
JAckGambateh <yeawjiaming04@gmail.com>
2026-03-26 03:36:36 +0800

    do file founting

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
+        </View>
+    )
+}
+export default Home
+
+const styles = StyleSheet.create({})

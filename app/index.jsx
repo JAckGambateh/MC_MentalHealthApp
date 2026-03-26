a9077d90d7179fb13a75a71990c01e831b6dcef7
JAckGambateh <yeawjiaming04@gmail.com>
2026-03-27 01:51:38 +0800

    add style

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/img/mentalHealthLogo.png'
 const Home = () => {
     return (
-        <View>
-            <Text>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
+        <View style={styles.container}>
+
+            <Image source={Logo} style={styles.img} />
+
+            <Text style={[styles.title, { color:'purple'}]}>The Numer 1</Text>
+            <Text style={{marginTop: 10, marinBottom: 30}}>aaaaaa</Text>
+
+            <View style={styles.card}>
+                <Text>Hello,mamamia</Text>
+            </View>
         </View>
     )
 }
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+    },
+    img:{
+        marginVertical: 20,
+    },
+    card: {
+        backgroundColor: '#dbd5d5',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: ' 4px 4px rgba(29, 1, 1, 0.1)',
+    },
+})
